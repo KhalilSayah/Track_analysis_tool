2f7e6ebfe463e933c20215383273566c79e5266f
SayahKhalil <sayahkhalil2001@gmail.com>
2026-01-12 19:10:11 +0100

    fixing slider

--- frontend/src/modules/Home/Home.jsx
+++ frontend/src/modules/Home/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
                     variants={container}
                     initial="hidden"
                     animate="show"
-                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+                    className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
                 >
                     {enabledModules.map((module) => (
                         <motion.div 
